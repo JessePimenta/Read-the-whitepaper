8f97168c499986a514ea809ba351ac6ba7b3e2ac
Jesse Pimenta <jpimenta@phenomenon.com>
2018-02-02 20:15:10 -0800

    first commit

---
+++ kirby-project/site/templates/project.php
@@ -0,0 +1,32 @@
+<?php snippet('header') ?>
+
+  <main class="main" role="main">
+
+    <header class="wrap">
+      <h1><?= $page->title()->html() ?></h1>
+      <div class="intro text">
+        <!-- <?= $page->year() ?> -->
+      </div>
+      <hr />
+    </header>
+
+    <div class="text wrap">
+      <?= $page->text()->kirbytext() ?>
+	      <?php if($document = $page->document()): ?>
+      <a href="<?php echo $document->url() ?>" download>Download</a>
+      <?php endif ?>
+
+      <?php
+      // Images for the "project" template are sortable. You
+      // can change the display by clicking the 'edit' button
+      // above the files list in the sidebar.
+      foreach($page->images()->sortBy('sort', 'asc') as $image): ?>
+      <?php endforeach ?>
+
+    </div>
+
+    <?php snippet('prevnext') ?>
+
+  </main>
+
+<?php snippet('footer') ?>
